3e95028370a757bebd54117cb98e9f279d02e46a
TowaSakurada <towatowa3939@icloud.com>
2026-07-17 17:37:03 +0900

    Initial Commit

---
+++ chameleon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chameleon
+//
+//  Created by 櫻田聖和 on 2026/07/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chameleon/chameleonApp.swift
@@ -0,0 +1,17 @@
+//
+//  chameleonApp.swift
+//  chameleon
+//
+//  Created by 櫻田聖和 on 2026/07/17.
+//
+
+import SwiftUI
+
+@main
+struct chameleonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
